7dab877d36ca4927d14855d37b080b9ad1905e69
bbell03 <bbell03@tufts.edu>
2019-12-14 14:30:16 -0500

    updated documentation

--- ANN.py
+++ ANN.py
@@ -1,36 +1,36 @@
 import numpy as np
 import pandas as pd
 
-
+#Returns true if input is Iris-setosa
 def encodersetosa(Class):
     if Class == "Iris-setosa":
         return 1
     else:
         return 0
 
-
+#Returns true if input is Iris-veriscolor
 def encoderversicolor(Class):
     if Class == "Iris-versicolor":
         return 1
     else:
         return 0
 
-
+#Returns true if input is Iris-virginica
 def encodervirginica(Class):
     if Class == "Iris-virginica":
         return 1
     else:
         return 0
 
-
+#sigmoid function on input x
 def sig(x):
     return (1 / (1 + np.exp(-x)))
 
-
+#derivative of sigmoid function on input y
 def derisig(y):
     return y * (1 - y)
 
-
+#declare class NeuralNetwork
 class NerualNetwork:
     Input_Size = 0
     Hidden_Size = 0
@@ -41,6 +41,7 @@ class NerualNetwork:
     b1 = None
     lr = None
 
+    #init function
     def __init__(self, input_layer_size, hidden_layer_size, output_layer_size, learning_rate):
         self.Input_Size = input_layer_size
         self.Hidden_Size = hidden_layer_size
@@ -52,9 +53,11 @@ class NerualNetwork:
         self.lr = learning_rate
         pass
 
+    #member function train takes inputs self, X, Y, iter_turns
     def train(self, X, y, iter_turns):
         for iter in range(iter_turns):
             for i in range(len(X)):
+
                 # forward propagation
                 self.l0 = X[i]
                 y_one = y[i]
@@ -69,8 +72,7 @@ class NerualNetwork:
                 self.w0 += np.dot(self.l0.reshape(self.Input_Size, 1), Delta1.reshape(1, self.Hidden_Size)) * self.lr
                 self.b0 += Delta1 * 1 * self.lr
 
-        print("Finish Training, IterTurn = " + str(iter_turns))
-
+    #predict function takes inputs self and X
     def predict(self, X):
         l0 = X
         l1 = sig(np.dot(l0, self.w0) + self.b0)
@@ -86,6 +88,7 @@ class NerualNetwork:
                 result.append([0, 0, 1])
         return np.array(result)
 
+    #predict_and_calculate_accuracy takes inputs self, X and y
     def predict_and_calculate_accuracy(self, X, y):
         result = self.predict(X).tolist()
         count = 0
@@ -94,6 +97,7 @@ class NerualNetwork:
                 count += 1
         return 1 - (count / len(result))
 
+    #predict_one takes inputs self and X, for use on single inputs
     def predict_one(self, X):
         result = ""
         l0 = [X]
@@ -109,22 +113,45 @@ class NerualNetwork:
                 result = "Iris-virginica"
         return result
 
+    #outputs value of Nerual Network
+    def output(self):
+        return self.w0, self.w1, self.b0, self.b1
 
+#main function
 if __name__ == "__main__":
+    #read data from input file
     df = pd.read_csv("ANN - Iris data.txt", header=None)
     df = df.rename(columns={0: "Sepal-Length", 1: "Sepal-Width", 2: "Petal-Length", 3: "Petal-Width", 4: "Class"})
 
+    #pass data from fie to encoder functiosn to determine plant type
     df["Iris-setosa"] = df["Class"].apply(encodersetosa)
     df["Iris-versicolor"] = df["Class"].apply(encoderversicolor)
     df["Iris-virginica"] = df["Class"].apply(encodervirginica)
 
-    y = df[["Iris-setosa", "Iris-versicolor", "Iris-virginica"]].values
-    X = df.drop(["Iris-setosa", "Iris-versicolor", "Iris-virginica", "Class"], axis=1).values
 
+    #strings results of encoder function together to result in a unique code for each plant type
+    # 1 0 0 = Iris-setosa, 0 1 0 = Iris-veriscolor, 0 0 1 = Iris-virginica
+    full_list = df.drop(["Class"], axis=1).values
+
+    np.random.shuffle(full_list)
+    train_list=full_list[:100]
+    test_list=full_list[100:]
+    X_train = train_list[:, :4]
+    y_train = train_list[:, 4:]
+    X_test = test_list[:, :4]
+    y_test = test_list[:, 4:]
+
+
+    #Initialize Neural Network with the following properties
     nn = NerualNetwork(input_layer_size=4, hidden_layer_size=6, output_layer_size=3, learning_rate=0.1)
-    nn.train(X, y, 1000)
 
-    print("Accuracy on trainset is : " + str(nn.predict_and_calculate_accuracy(X, y)))
+    #Train NN with data from dataset
+    nn.train(X_train, y_train, 1000)
 
+    #Print accuracy
+    print("Accuracy on trainset is : " + str(nn.predict_and_calculate_accuracy(X_train, y_train)))
+    print("Accuracy on test is : " + str(nn.predict_and_calculate_accuracy(X_test, y_test)))
+
+    #Arr with data to be passed to predict function
     predict_data=[4.8, 3.4, 1.6, 0.2]
     print("If an iris flower have X = {} , It is an {}".format(str(predict_data),nn.predict_one(predict_data)))
--- README.md
+++ README.md
@@ -11,60 +11,49 @@
   #### IMPLEMENTATION:
   We expect that our solution has been completely and correctly implemented. The code was written in Python 3.
 
-
   #### ARCHITECTURE:
 
   Our solution makes use of an Artificial Neural Network in order to classify plants according to values corresponding to the length and width of both their petals and sepals.
 
-  According to standard procedure, our Neural Network trains on the dataset ANN -- Iris data.txt using forwards propogation followed by backwards propogation to generalize attributes for each type of plant so as to predict them on input.
+  According to standard procedure, our Neural Network trains on the dataset ANN -- Iris data.txt using forward propogation followed by backward propogation to generalize attributes for each type of plant so as to predict type on attribute input.
 
   We use various helper functions in addition to a Neural Network class complete
   with its own member functions to implement the solution.
 
+  Our Neural Network randomly selects 100/150 data entries in order to train itself, and then tests the validity of the training process on the remaining
+  100 individuals.
+
+  Helper functions include boolean return functions for each of the three types of plant, and a sigmoid function that serves as the activation function for this artificial neural net.
+
+  We also make use of python libraries numpy and pandas for mathematical operations.
+
   #### FUNCTION(S) IMPLEMENTED INCLUDE:
 
-  Class Neural Network:
-    Attributes:
-      Input_Size = 0
-      Hidden_Size = 0
-      Output_Size = 0
-
-      #Clarify
-      w0 = None
-      w1 = None
-      b0 = None
-      b1 = None
-      lr = None
-
-    Member Functions:
+  Class Member Functions:
       train
+      - trains the neural net with 100 individuals randomly selected
+        from data set using forward propogation and backward propogation
       predict
+      - uses training data to predict plant type based on length and width of
+        petals and sepals
       predict_and_calculate_accuracy
+      - uses training data to predict plant type and calculates accuracy
       predict_one
 
-       #Returns true if input is Iris-setosa
-       def encodersetosa(Class):
+      encodersetosa(Class)
+      - Returns true if input is Iris-setosa
 
+      encoderversicolor(Class)
+      - Returns true if input is Iris-veriscolor
 
-       #Returns true if input is Iris-veriscolor
-       encoderversicolor(Class):
+      encodervirginica(Class)
+      - Returns true if input is Iris-virginica
 
+      sig(x)
+        - Sigmoid activation function
 
-       #Returns true if input is Iris-virginica
-       encodervirginica(Class):
-           if Class == "Iris-virginica":
-               return 1
-           else:
-               return 0
-
-       #sigmoid function on input x
-       sig(x):
-           return (1 / (1 + np.exp(-x)))
-
-       #derivative of sigmoid function on input y
-       derisig(y):
-           return y * (1 - y)
-
+      derisig(y)
+        - Derivative of sigmoid function on input y
 
   #### METHODOLOGY:
     Our program is implemented according to the ANN guidelines
